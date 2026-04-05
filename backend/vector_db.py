1c0767e2f24fd5fd79efafb932cfa05e262a3bbf
PURUSHOTHAMA-CM <purushothamacm1011@gmail.com>
2026-04-05 18:44:25 +0530

    Update vector_db.py to use cache folder for model

--- backend/vector_db.py
+++ backend/vector_db.py
@@ -2,20 +2,22 @@ import chromadb
 from sentence_transformers import SentenceTransformer
 import os
 
+os.environ["HF_HOME"]="/tmp"
+
 # Load model once
 model = None
 
 def get_model():
     global model
     if model is None:
-        from sentence_transformers import SentenceTransformer
         print("📦 Loading model...")
-        model = SentenceTransformer('all-MiniLM-L6-v2')
+        model = SentenceTransformer('all-MiniLM-L6-v2',cache_folder="/tmp")
     return model
 
 BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+DB_PATH=os.path.join(BASE_DIR,"chroma_db)
 
 # Persistent DB (important)
-client = chromadb.PersistentClient(path="./chroma_db")
+client = chromadb.PersistentClient(path=DB_PATH)
 
 collection = client.get_or_create_collection(name="dashboards")
