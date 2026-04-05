06762447331033bc37372f720cf436355f22c778
PURUSHOTHAMA-CM <purushothamacm1011@gmail.com>
2026-04-05 18:37:10 +0530

    seperate query engine and api

--- backend/query_engine.py
+++ backend/query_engine.py
@@ -1,29 +1,6 @@
-from fastapi import FastAPI
 from vector_db import collection, get_model
-from fastapi.middleware.cors import CORSMiddleware
-from script import load_data
 
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.on_event("startup")
-def startup_event():
-    load_data()
-
-@app.get("/")
-def home():
-    return {"message":"API is running"}
-
-@app.get("/search")
 def search_dashboard(query: str):
 
     model = get_model()
